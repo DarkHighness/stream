841d2071ba61ffb4b1384855c9de0c56363d9c94
Twiliness <729294652hs@gmail.com>
2021-12-26 19:48:49 +0800

    b version done

--- src/B/Stream.hs
+++ src/B/Stream.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE RankNTypes #-}
+
 module B.Stream where
 
+import Control.Applicative (Alternative (empty, (<|>)), Applicative (liftA2))
+import Control.Monad
 import qualified Control.Monad.Fail as Fail
-import Control.Monad.Trans ( MonadTrans(..) )
-import Control.Applicative ()
+import Control.Monad.Trans (MonadTrans (..))
 
 data Stream f m r
   = Step !(f (Stream f m r))
@@ -45,5 +48,54 @@ instance (Functor f, Monad m) => Applicative (Stream f m) where
       go (Effect m) = Effect (fmap go m)
       go (Step f) = Step (fmap go f)
 
+instance (Functor f, Monad m, Semigroup w) => Semigroup (Stream f m w) where
+  a <> b = do
+    w <- a
+    fmap (w <>) b
+
+instance (Functor f, Monad m, Monoid w) => Monoid (Stream f m w) where
+  mempty = return mempty
+
+  mappend a b = do
+    w <- a
+    fmap (w `mappend`) b
+
+instance (Applicative f, Monad m) => Alternative (Stream f m) where
+  empty = never
+  s1 <|> s2 = zipWith' liftA2 s1 s2
+
+instance (Applicative f, Monad m) => MonadPlus (Stream f m) where
+  mzero = empty
+  mplus = (<|>)
+
 instance Functor f => MonadTrans (Stream f) where
-    lift = Effect . fmap Return
+  lift = Effect . fmap Return
+
+zipWith ::
+  forall f g h m r.
+  (Monad m, Functor h) =>
+  (forall x y. f x -> g y -> h (x, y)) ->
+  Stream f m r ->
+  Stream g m r ->
+  Stream h m r
+zipWith phi = zipWith' $ \xyp fx gy -> uncurry xyp <$> phi fx gy
+
+zipWith' ::
+  forall f g h m r.
+  Monad m =>
+  (forall x y p. (x -> y -> p) -> f x -> g y -> h p) ->
+  Stream f m r ->
+  Stream g m r ->
+  Stream h m r
+zipWith' phi = go
+  where
+    go s t = case s of
+      Return r -> Return r
+      Step fs -> case t of
+        Return r -> Return r
+        Step gs -> Step $ phi go fs gs
+        Effect n -> Effect $ fmap (go s) n
+      Effect m -> Effect $ fmap (`go` t) m
+
+never :: (Monad m, Applicative f) => Stream f m r
+never = let fix = Step $ pure (Effect (return fix)) in fix
